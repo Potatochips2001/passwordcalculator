60f47e13a448b46fd8b501ebfae6aba6616d3a5b
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2021-02-27 23:15:15 -0500

    passwordtime.py
    Estimate password cracking time(s)

--- passwordtime.py
+++ passwordtime.py
@@ -1,11 +1,18 @@
 import random
 import datetime
+import sys
 
 
 print("\n")
-passw = input("Password: ")
-comb = len(set(passw)) ** len(passw)
-crackTime = comb/2000
+
+if len(sys.argv) > 1:
+    passw = sys.argv[1]
+    comb = len(set(passw))**len(passw)
+    crackTime = comb / 2000
+else:
+    passw = input("Password: ")
+    comb = len(set(passw)) ** len(passw)
+    crackTime = comb/2000
 
 try:
     print(f"\nTime to crack password : {str(datetime.timedelta(seconds=crackTime))}\n")
