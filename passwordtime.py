13c4ee0b62064942e2ac7e537aede34f8268c300
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2021-02-28 14:20:08 -0500

    passwordtime.py
    Calculate time to crack password.

--- passwordtime.py
+++ passwordtime.py
@@ -5,18 +5,26 @@ import sys
 
 print("\n")
 
-if len(sys.argv) > 1:
-    passw = sys.argv[1]
-    comb = len(set(passw))**len(passw)
-    crackTime = comb / 2000
-else:
-    passw = input("Password: ")
-    comb = len(set(passw)) ** len(passw)
-    crackTime = comb/2000
+try:
+    if len(sys.argv) > 1:
+        passw = sys.argv[1]
+        comb = len(set(passw))**len(passw)
+        crackTime = comb / 2000
+    else:
+        passw = input("Password: ")
+        comb = len(set(passw)) ** len(passw)
+        crackTime = comb/2000
+except OverflowError:
+    print("\nNever\n")
+except:
+    pass
 
 try:
     print(f"\nTime to crack password : {str(datetime.timedelta(seconds=crackTime))}\n")
 except OverflowError:
-    print("\n>68 years")
+    try:
+        print(f'\nTime to crack password : {str(crackTime/86400/365)} years')
+    except:
+        print('\nNever')
 except:
     pass
